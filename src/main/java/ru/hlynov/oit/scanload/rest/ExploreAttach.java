edb0e69fa53201dd5da7461cf85a5f6b8b3a7b01
alex k <kaanova@gmail.com>
2019-02-28 14:42:27 +0300

    add rest and http client

--- src/main/java/ru/hlynov/oit/scanload/rest/ExploreAttach.java
+++ src/main/java/ru/hlynov/oit/scanload/rest/ExploreAttach.java
@@ -6,6 +6,12 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +19,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
+
+import java.io.IOException;
 
 /**
  * A resource of message.
@@ -31,13 +39,83 @@ public class ExploreAttach {
     public Response getMessage()
     {
         Gson gson = new Gson();
-        JsonArray jsonArray = new JsonArray();
+//        JsonArray jsonArray = new JsonArray();
+//
+//        JsonObject jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "rc_display.json");
+//        jsonObject.addProperty("size", "272");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "fernflower.jar");
+//        jsonObject.addProperty("size", "245371");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "Образец письма без логотипа (1).docx");
+//        jsonObject.addProperty("size", "245371");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "rc_asm.json");
+//        jsonObject.addProperty("size", "338");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "rclog.txt");
+//        jsonObject.addProperty("size", "894");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "Необходимые+самостоятельные+доработки (1).doc");
+//        jsonObject.addProperty("size", "122135");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "rc_blocks.json");
+//        jsonObject.addProperty("size", "29");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "ОписаниеМодулейДжиры.doc");
+//        jsonObject.addProperty("size", "23552");
+//        jsonArray.add(jsonObject);
+//
+//        jsonObject = new JsonObject();
+//        jsonObject.addProperty("name", "rc_cfr.json");
+//        jsonObject.addProperty("size", "1628");
+//        jsonArray.add(jsonObject);
+//
 
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("name", "rc_display.json");
-        jsonObject.addProperty("size", "272");
-        jsonArray.add(jsonObject);
 
-        return Response.ok(gson.toJson(jsonObject)).build();
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet("http://localhost/api/getDirectoryContent.php");
+        request.addHeader("accept", "application/json");
+
+        HttpResponse response;
+
+        try {
+            response = client.execute(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+        }
+
+        String jsonString = "[]";
+        try {
+            jsonString = IOUtils.toString(response.getEntity().getContent());
+        } catch (IOException e) {
+            e.printStackTrace();
+            jsonString = "[]";
+        }
+
+//        if (jsonString == null) {
+//            log.warn(" ================== no get json");
+//        } else {
+//            log.warn(jsonString);
+//        }
+
+        return Response.ok(jsonString).build();
+
     }
 }
--- src/main/resources/js/scanload.js
+++ src/main/resources/js/scanload.js
@@ -8,28 +8,50 @@ scanload.module = (function () {
 
     var refresh = function() {
 
-        console.log("============= refresh =============");
-        var jsonText = "[{\"name\":\"rc_display.json\",\"size\":272},{\"name\":\"fernflower.jar\",\"size\":245371},{\"name\":\"\u041e\u0431\u0440\u0430\u0437\u0435\u0446 \u043f\u0438\u0441\u044c\u043c\u0430 \u0431\u0435\u0437 \u043b\u043e\u0433\u043e\u0442\u0438\u043f\u0430 (1).docx\",\"size\":22800},{\"name\":\"rc_asm.json\",\"size\":338},{\"name\":\"rclog.txt\",\"size\":894},{\"name\":\"\u041d\u0435\u043e\u0431\u0445\u043e\u0434\u0438\u043c\u044b\u0435+\u0441\u0430\u043c\u043e\u0441\u0442\u043e\u044f\u0442\u0435\u043b\u044c\u043d\u044b\u0435+\u0434\u043e\u0440\u0430\u0431\u043e\u0442\u043a\u0438 (1).doc\",\"size\":122135},{\"name\":\"rc_blocks.json\",\"size\":29},{\"name\":\"\u041e\u043f\u0438\u0441\u0430\u043d\u0438\u0435\u041c\u043e\u0434\u0443\u043b\u0435\u0439\u0414\u0436\u0438\u0440\u044b.doc\",\"size\":23552},{\"name\":\"rc_cfr.json\",\"size\":1628}]"
-        var jsonObj = JSON.parse(jsonText);
-        console.log(jsonObj);
+        // console.log("============= refresh =============");
 
-        var json_size = jsonObj.length;
-        var rowStr = "";
 
-        // таблица
-        var tableObj = AJS.$("table#filesOnLocalSide tbody");
-        // очистка таблицы
-        tableObj.empty();
+        // if (AJS.$("table#filesOnLocalSide tbody").length == 0) {
+        //     return false;
+        // }
 
-        for (var i = 0; i < json_size; i++) {
-            // заполняем значения строки
-            rowStr = rowTemplate.replace("__name__", jsonObj[i].name);
-            rowStr = rowStr.replace("__size__", jsonObj[i].size);
 
-            // добавляем строку
-            tableObj.append(rowStr);
-        }
+        AJS.$.ajax({
+            url: AJS.params.baseURL + "/rest/exploreattach/1.0/scanload/getdirectorycontent",
+            type: 'get',
+            dataType: 'json',
+            async: true,
+            success: function(data) {
 
+                // console.log("============= refresh =============");
+                // var jsonText = "[{\"name\":\"rc_display.json\",\"size\":272},{\"name\":\"fernflower.jar\",\"size\":245371},{\"name\":\"\u041e\u0431\u0440\u0430\u0437\u0435\u0446 \u043f\u0438\u0441\u044c\u043c\u0430 \u0431\u0435\u0437 \u043b\u043e\u0433\u043e\u0442\u0438\u043f\u0430 (1).docx\",\"size\":22800},{\"name\":\"rc_asm.json\",\"size\":338},{\"name\":\"rclog.txt\",\"size\":894},{\"name\":\"\u041d\u0435\u043e\u0431\u0445\u043e\u0434\u0438\u043c\u044b\u0435+\u0441\u0430\u043c\u043e\u0441\u0442\u043e\u044f\u0442\u0435\u043b\u044c\u043d\u044b\u0435+\u0434\u043e\u0440\u0430\u0431\u043e\u0442\u043a\u0438 (1).doc\",\"size\":122135},{\"name\":\"rc_blocks.json\",\"size\":29},{\"name\":\"\u041e\u043f\u0438\u0441\u0430\u043d\u0438\u0435\u041c\u043e\u0434\u0443\u043b\u0435\u0439\u0414\u0436\u0438\u0440\u044b.doc\",\"size\":23552},{\"name\":\"rc_cfr.json\",\"size\":1628}]"
+                // var jsonObj = JSON.parse(jsonText);
+                var jsonObj = data;
+                // console.log(jsonObj);
+
+                var json_size = jsonObj.length;
+                var rowStr = "";
+
+                // таблица
+                var tableObj = AJS.$("table#filesOnLocalSide tbody");
+                // очистка таблицы
+                tableObj.empty();
+
+                for (var i = 0; i < json_size; i++) {
+                    // заполняем значения строки
+                    rowStr = rowTemplate.replace("__name__", jsonObj[i].name);
+                    rowStr = rowStr.replace("__size__", jsonObj[i].size);
+
+                    // добавляем строку
+                    tableObj.append(rowStr);
+                }
+
+                // console.log(data);
+                // user = data.username;
+            }
+        });
+
+        return true;
 
     }
 
@@ -48,6 +70,14 @@ scanload.module = (function () {
 // AJS.$(function () {
 AJS.toInit(function() {
 
+
+    // запуск обновления списка файлов
+
+    setInterval(function(){
+        if (AJS.$("table#filesOnLocalSide tbody").length != 0) {
+            scanload.module.refresh();
+        }
+    } , 5000);
 
     // console.log("===================");
     // console.log(AJS.$("#button-refresh"));
