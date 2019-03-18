bca29301b6279f51aa739ffc913d290e6d0fbeb5
alex k <kaanova@gmail.com>
2019-03-18 15:01:15 +0300

    tuning table creator

--- src/main/resources/js/scanload.js
+++ src/main/resources/js/scanload.js
@@ -17,6 +17,28 @@ scanload.module = (function () {
 
 
     ////////////////////////////////////////////
+    // установить блокировку на выполнение операций
+    ////////////////////////////////////////////
+    var isAccessLocked = function() {
+        return lockOperations;
+    }
+
+    var tryAccessLock = function() {
+        if (!lockOperations) {
+            lockOperations = true;
+            return true;
+        }
+        return false;
+    }
+
+    var setAccessUnlock = function() {
+        lockOperations = false;
+        return true;
+    }
+
+
+
+    ////////////////////////////////////////////
     // получить текущие вложения задачи
     ////////////////////////////////////////////
     var issueAttachments = function(issueId) {
@@ -53,6 +75,141 @@ scanload.module = (function () {
     }
 
 
+
+    ////////////////////////////////////////////
+    // проверить отличаются ли файлы в таблице
+    // и файлы полученные с сервера
+    ////////////////////////////////////////////
+    // true - changed, false - not changed
+    var tableISChanged = function(jsonData) {
+
+        // console.log("=================================================================");
+        // console.log("проверка того что те же файлы на сервере и в задаче в панели загрузки");
+        //
+        // console.log("данные полученные с сервера");
+        // console.log(jsonData);
+
+        // пробегаем по jsonData полученному с сервера и сравниваем их с таблицей на клиенте
+        // если что то не сходится то сразу возвращаем true
+
+
+        var tableObj = AJS.$("table#filesOnLocalSide tbody tr");
+        var tableSize = tableObj.size();
+
+        // если количество не сходится то безоговорочно есть изменения
+        if (jsonData.length != tableSize) {
+            return true;
+        }
+
+
+        // возвращаемое значение
+        var retVal = true;
+
+
+        var tableFilename = "";    // имя файла в таблице
+        var tableFilesize = "";    // размер файла в таблице
+
+        // проходим по jsonData
+        for (var i = 0; i < jsonData.length; i++) {
+            // console.log("====== " + jsonData[i].name + " ====== " + jsonData[i].size);
+
+            retVal = true;
+
+            // проходим по таблице
+            for (var j = 0; j < tableSize; j++) {
+                tableFilename = AJS.$(tableObj[j]).find(".column_1").text();
+                tableFilesize = AJS.$(tableObj[j]).find(".column_2").text();
+
+                //
+                if ((jsonData[i].name == tableFilename) && (jsonData[i].size == tableFilesize)) {
+                    retVal = false;
+                }
+            }
+
+            if (retVal) {
+                break;
+            }
+
+        }
+
+        // AJS.$(AJS.$("table#filesOnLocalSide tbody tr")[1]).find(".column_2").text()
+
+        return retVal;
+    }
+
+
+
+    ////////////////////////////////////////////
+    // проверить есть ли файлы из таблицы
+    // во вложениях, если есть то покрасить
+    // строку красными, иначе обычным
+    ////////////////////////////////////////////
+    var colorizedRows = function() {
+
+        // var json_size = jsonObj.length;
+
+        // получим существующие вложения
+        var issueIdObj = AJS.$("a#key-val");
+        // массив с вложениями в формате JSON obj
+        var issueAttachsJson = [];
+
+        if (issueIdObj.length == 1) {
+            issueAttachsJson = issueAttachments(issueIdObj.text());
+        }
+
+        // признак того что имя файла найдено во вложениях
+        // var findInAttach = false;
+
+
+        var currAttachLength = issueAttachsJson.length;
+
+
+        var tableObj = AJS.$("table#filesOnLocalSide tbody tr");
+        var tableSize = tableObj.size();
+
+        var tableFilename = "";    // имя файла в таблице
+        var tableFilesize = "";    // размер файла в таблице
+
+
+        for (var i = 0; i < currAttachLength; i++) {
+
+            console.log(" =================== ");
+            console.log("issueAttachsJson[i] ");
+            console.log(issueAttachsJson[i]);
+
+            // проходим по таблице
+            for (var j = 0; j < tableSize; j++) {
+                tableFilename = AJS.$(tableObj[j]).find(".column_1").text();
+                tableFilesize = AJS.$(tableObj[j]).find(".column_2").text();
+
+                //
+                // if ((issueAttachsJson[ii].filename == tableFilename) && (jsonData[i].size == tableFilesize)) {
+                if ((issueAttachsJson[i].filename == tableFilename) && (issueAttachsJson[i].size == tableFilesize)) {
+                    // нашли во вложениях - закрасить строку красным
+
+                } else {
+                    // не нашли во вложениях - закрасить строку черным
+
+                }
+            }
+
+
+
+            //
+            // //ищем среди текущих вложений
+            // findInAttach = false;
+            // for (var ii = 0; ii < currAttachLength; ii++) {
+            //     if (jsonObj[i].name == issueAttachsJson[ii].filename) {
+            //         findInAttach = true;
+            //     }
+            // }
+
+        }
+
+    }
+
+
+
     ////////////////////////////////////////////
     // заполнить таблицу файлов в папке загрузки
     ////////////////////////////////////////////
@@ -65,6 +222,10 @@ scanload.module = (function () {
         //     return false;
         // }
 
+        // if (!tryAccessLock()) {
+        //     return true;
+        // }
+
 
         AJS.$.ajax({
             url: AJS.params.baseURL + "/rest/exploreattach/1.0/scanload/getdirectorycontent",
@@ -72,6 +233,14 @@ scanload.module = (function () {
             dataType: 'json',
             async: true,
             success: function(data) {
+
+
+                if (!tableISChanged(data)) {
+                    // обновим цвета строк
+                    colorizedRows();
+                    // возврат
+                    return;
+                }
 
                 // console.log("============= refresh =============");
 
@@ -244,6 +413,9 @@ scanload.module = (function () {
 
                     console.log(data);
 
+                    // обновление окна задачи
+                    JIRA.trigger(JIRA.Events.REFRESH_ISSUE_PAGE, [JIRA.Issue.getIssueId()]);
+
                     // console.log(data);
                     // user = data.username;
                 },
@@ -289,7 +461,7 @@ AJS.toInit(function() {
         if (AJS.$("table#filesOnLocalSide tbody").length != 0) {
             scanload.module.refresh();
         }
-    } , 5000);
+    } , 4000);
 });
 
 
